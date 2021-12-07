817a7fcd0235a9dee0615cba60b5ae41f52f4cda
Felipe Martenexen <felipe.martenexen@gmail.com>
2021-12-07 16:28:59 -0300

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1tDfUz5gUcXiZ3ZBgQR8dzcfecfxxXnBTPUIzBC6dN9g/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyBx_Iev75ftGHPvgSVmJfkBHD-ShjY3coc';
